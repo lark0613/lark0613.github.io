50cb1346e2b1aef70b7bb505b814688a697449d6
lark0613 <lark_0613@163.com>
2025-11-27 16:50:14 +0800

    v1.2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function updateClock() {
   const now = new Date();
-  document.getElementById('clock').textContent = now.toLocaleTimeString('zh-CN', {hour12: false});
-  document.getElementById('date').textContent = now.toLocaleDateString('zh-CN', {weekday:'long', year:'numeric', month:'long', day:'numeric'});
+  document.getElementById('clock').textContent = now.toLocaleTimeString('zh-CN', { hour12: false });
+  document.getElementById('date').textContent = now.toLocaleDateString('zh-CN', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 }
 setInterval(updateClock, 1000);
 updateClock();
